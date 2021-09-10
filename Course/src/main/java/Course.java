95aba0a7c7ab98205ebb43a91ca15aff22fd392d
julius <julius.r.wilson@gmail.com>
2021-09-10 12:52:01 -0400

    added from laptop to check if git is working.

--- Course/src/main/java/Course.java
+++ Course/src/main/java/Course.java
@@ -10,4 +10,15 @@
  */
 public class Course {
     
+    public static void main (String [] args)
+    {
+        System.out.println("Hello! ");    
+    }
+    
+    
+    
+    
+    
+    
+    
 }
